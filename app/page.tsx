bdb56332e5e4065b045b8f591120932ff01553d3
h5365jheqhjy6ydrt4 <123633582+h5365jheqhjy6ydrt4@users.noreply.github.com>
2024-06-07 08:08:23 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
         <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
           <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
-            Get updates of out journey!
+            Follow our journey!
           </h1>
           <p className="text-gray-500">
             Join the waitlist to be notified of updates!
